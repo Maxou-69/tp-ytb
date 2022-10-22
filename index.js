c3ea4cd6518a12039b14e08beaf9f814b1f062eb
Maxou <leroymax2006@gmail.com>
2022-10-22 11:22:33 +0200

    darkmode

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ console.log("js fonctionnel")
 
 const body = document.querySelector("body");
 const titres = document.querySelectorAll("h3");
-const iframe = document.querySelectorAll("iframe");
+const iframes = document.querySelectorAll("iframe");
 const checkbox = document.querySelector("#checkbox");
 const navbar = document.querySelector("nav");
 
@@ -10,6 +10,12 @@ checkbox.addEventListener('click', function () {
 
     body.classList.toggle("darkmode");
     navbar.classList.toggle("darkmode");
+    titres.forEach(titre => {
+        titre.classList.toggle("darkmode");
+    })
+    iframes.forEach(iframe => {
+        iframe.classList.toggle("darkmode");
+    })
 
     console.log("bouton fonctionnel")
 
