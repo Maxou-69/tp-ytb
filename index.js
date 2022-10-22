83337111cc455f58eaf9c355b33e3ea90de67dd7
Maxou <leroymax2006@gmail.com>
2022-10-22 11:06:15 +0200

    darkmode working now

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ console.log("js fonctionnel")
 const body = document.querySelector("body");
 const titres = document.querySelectorAll("h3");
 const iframe = document.querySelectorAll("iframe");
-const checkbox = document.getElementById("checkbox");
+const checkbox = document.querySelector("#checkbox");
+const navbar = document.querySelector("nav");
 
 checkbox.addEventListener('click', function () {
+
+    body.classList.toggle("darkmode");
+    navbar.classList.toggle("darkmode");
+
+    console.log("bouton fonctionnel")
 
 })
