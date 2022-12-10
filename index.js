a8008119391e07c55a1ce9ed07f875e3afd426c6
Maxou <leroymax2006@gmail.com>
2022-12-10 10:11:57 +0100

    iframe

--- index.js
+++ index.js
@@ -20,3 +20,30 @@ checkbox.addEventListener('click', function () {
     console.log("bouton fonctionnel")
 
 })
+
+//  1. Enregistrer une video youtube dans le localStorage
+function save() {
+    let iframe = document.getElementById("iframecode").value
+    window.localStorage.setItem("iframe" + (+new Date()), iframe);
+    window.location.reload()
+};
+
+// 2. Chargement des videos enregistrés
+window.onload = function load() {
+    let iframes = { ...localStorage };
+    let html = "";
+    for ([key, iframe] of Object.entries(iframes)) {
+        html += iframe + `<button class="delete" style="position:relative; left:-30px; top:-10px" data-key="${key}">X</button>`
+
+    };
+
+    document.querySelector('.video-play').innerHTML = html;
+
+    document.querySelectorAll('button.delete').forEach((button) => {
+        button.addEventListener("click", () => {
+            window.localStorage.removeItem(button.dataset.key);
+            window.location.reload();
+        })
+    })
+
+}
